2394e24c2cc07d2ae704cb8d7950a777f9a02979
Robert Nitsch <r.s.nitsch@gmail.com>
2012-10-14 15:04:10 +0200

    First version of raw template.

--- main.php
+++ main.php
@@ -28,51 +28,35 @@ if (!defined('DOKU_INC')) die();
   </title>
 
   <?php tpl_metaheaders()?>
-  <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
+  <link rel="shortcut icon" href="<?php echo tpl_getFavicon() ?>" />
 
-  <?php /*old includehook*/ @include(dirname(__FILE__).'/meta.html')?>
+  <?php @include(dirname(__FILE__).'/meta.html')?>
 </head>
 
 <body>
 <?php /*old includehook*/ @include(dirname(__FILE__).'/topheader.html')?>
 <div class="dokuwiki">
+
   <?php html_msgarea()?>
+
+  <h1><?php tpl_link(wl(),$conf['title'],'name="dokuwiki__top" id="dokuwiki__top" accesskey="h" title="[H]"')?></h1>
+
+  <div class="navi">
+    <?php tpl_include_page("navi")?>
+  </div>
 
   <div class="stylehead">
 
+    <?php if (false): ?>
     <div class="header">
       <div class="pagename">
-        [[<?php tpl_link(wl($ID,'do=backlink'),tpl_pagetitle($ID,true),'title="'.$lang['btn_backlink'].'"')?>]]
+        <?php #echo tpl_pagetitle($ID,true)?>
       </div>
-      <div class="logo">
-        <?php tpl_link(wl(),$conf['title'],'name="dokuwiki__top" id="dokuwiki__top" accesskey="h" title="[H]"')?>
-      </div>
-
       <div class="clearer"></div>
     </div>
+    <?php endif; ?>
 
     <?php /*old includehook*/ @include(dirname(__FILE__).'/header.html')?>
-
-    <div class="bar" id="bar__top">
-      <div class="bar-left" id="bar__topleft">
-        <?php tpl_button('edit')?>
-        <?php tpl_button('history')?>
-      </div>
-
-      <div class="bar-right" id="bar__topright">
-        <?php tpl_button('recent')?>
-        <?php tpl_searchform()?>&nbsp;
-      </div>
-
-      <div class="clearer"></div>
-    </div>
-
-    <?php if($conf['breadcrumbs']){?>
-    <div class="breadcrumbs">
-      <?php tpl_breadcrumbs()?>
-      <?php //tpl_youarehere() //(some people prefer this)?>
-    </div>
-    <?php }?>
 
     <?php if($conf['youarehere']){?>
     <div class="breadcrumbs">
@@ -97,24 +81,12 @@ if (!defined('DOKU_INC')) die();
 
   <div class="stylefoot">
 
-    <div class="meta">
-      <div class="user">
-        <?php tpl_userinfo()?>
-      </div>
-      <div class="doc">
-        <?php tpl_pageinfo()?>
-      </div>
-    </div>
-
-   <?php /*old includehook*/ @include(dirname(__FILE__).'/pagefooter.html')?>
+    <?php /*old includehook*/ @include(dirname(__FILE__).'/pagefooter.html')?>
 
     <div class="bar" id="bar__bottom">
-      <div class="bar-left" id="bar__bottomleft">
         <?php tpl_button('edit')?>
         <?php tpl_button('history')?>
         <?php tpl_button('revert')?>
-      </div>
-      <div class="bar-right" id="bar__bottomright">
         <?php tpl_button('subscribe')?>
         <?php tpl_button('media')?>
         <?php tpl_button('admin')?>
@@ -122,8 +94,6 @@ if (!defined('DOKU_INC')) die();
         <?php tpl_button('login')?>
         <?php tpl_button('index')?>
         <?php tpl_button('top')?>&nbsp;
-      </div>
-      <div class="clearer"></div>
     </div>
 
   </div>
@@ -131,7 +101,7 @@ if (!defined('DOKU_INC')) die();
   <?php tpl_license(false);?>
 
 </div>
-<?php /*old includehook*/ @include(dirname(__FILE__).'/footer.html')?>
+<?php @include(dirname(__FILE__).'/footer.html')?>
 
 <div class="no"><?php /* provide DokuWiki housekeeping, required in all templates */ tpl_indexerWebBug()?></div>
 </body>
